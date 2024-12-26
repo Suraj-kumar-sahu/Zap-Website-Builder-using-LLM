069faaa2e73ae68a8f833aef27104370684357ab
Surajkumarrr <surajsahu13sk@gmail.com>
2024-12-26 23:46:55 +0530

    Used the Anthropic-AI SDK to test Claude API

---
+++ Backend/src/index.ts
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+import Anthropic from "@anthropic-ai/sdk";
+
+const anthropic = new Anthropic();
+
+async function main() {
+    const msg = await anthropic.messages.create({
+        model: "claude-3-5-sonnet-20241022",
+        max_tokens: 1000,
+        temperature: 1,
+        messages: [{
+            "role": "user",
+            "content": "what is 2 + 2 ?"
+        }]
+      });
+      console.log(msg);
+}
+
+main();
