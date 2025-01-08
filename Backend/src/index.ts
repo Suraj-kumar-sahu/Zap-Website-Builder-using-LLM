d41a93f13c4bfaae4ad24114f104c888f28279fc
Surajkumarrr <surajsahu13sk@gmail.com>
2025-01-08 11:27:39 +0530

    Done with the Builder section

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,16 +1,15 @@
 require("dotenv").config();
-
 import express from "express";
 import Anthropic from "@anthropic-ai/sdk";
 import { BASE_PROMPT, getSystemPrompt } from "./prompts";
 import { ContentBlock, TextBlock } from "@anthropic-ai/sdk/resources";
 import {basePrompt as nodeBasePrompt} from "./defaults/node";
 import {basePrompt as reactBasePrompt} from "./defaults/react";
-// import cors from "cors";
+import cors from "cors";
 
 const anthropic = new Anthropic();
 const app = express();
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 
 app.post("/template", async (req, res) => {
